32dba38d4586a46bceb6867d28961e1f51dfc30d
nelson1987 <nelson.ash@outlook.com>
2020-09-27 20:02:36 -0300

    add a heading

--- aula.js
+++ aula.js
@@ -4,3 +4,21 @@ let peso = 86.4;
 let estaAprovado = true;
 let sobrenome = undefined;
 let corSelecionada = null;
+
+let pessoa = {
+    nome : 'Nelson',
+    idade : 33,
+    peso : 86.4,
+    estaAprovado : true,
+    sobrenome : 'neto'
+};
+console.log(pessoa);
+
+//Arrays
+let familia = [1,1];
+console.log(familia.length);
+console.log(familia[0]);
+
+let numero :number;
+= 100;
+console.log(numero);
